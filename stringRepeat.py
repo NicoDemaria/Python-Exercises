1187e5f941725175ac1ed10aa57d9352459407a5
Nicolas <nicolasdemaria4@gmail.com>
2022-09-07 17:28:07 -0300

    PythonEjercices

---
+++ stringRepeat.py
@@ -0,0 +1,11 @@
+'''Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+'''
+
+
+def repeat_str(repeat, string):
+    return string * repeat
+
+
+guardar = repeat_str(5, 'hello')
+print(guardar)
