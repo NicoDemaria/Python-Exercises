b67906b8596c6ea19da773cf8a0aa78ef19219b5
Nicolas <nicolasdemaria4@gmail.com>
2022-09-07 17:39:20 -0300

    Create stripfunc.py

---
+++ stripfunc.py
@@ -0,0 +1,9 @@
+'''Simple, remove the spaces from the string, then return the resultant string.
+'''
+
+
+def no_space(x):
+    return x.replace(' ', '')
+
+
+print(no_space('8 j 8   mBliB8g  imjB8B8  jl  B'))
