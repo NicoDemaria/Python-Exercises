9c4997d16f73a697c2ecb5f417edca42570f0cdb
Nicolas <nicolasdemaria4@gmail.com>
2022-11-23 22:44:29 -0300

    Update calcu.py

--- calcu.py
+++ calcu.py
@@ -4,4 +4,25 @@ a and b will both be positive integers, and a will always be the first number in
 
 The four operators are "add", "subtract", "divide", "multiply".
 
-A few examples:(Input1, Input2, Input3 --> Output)'''
+A few examples:(Input1, Input2, Input3 --> Output)
+
+5, 2, "add"      --> 7
+5, 2, "subtract" --> 3
+5, 2, "multiply" --> 10
+5, 2, "divide"   --> 2.5
+
+'''
+
+
+def arithmetic(a, b, operator):
+    if operator == "add":
+        return a + b
+    elif operator == "subtract":
+        return a - b
+    elif operator == "multiply":
+        return a * b
+    elif operator == "divide":
+        return a / b
+
+
+arithmetic(5, 2, "add")  # => 7
