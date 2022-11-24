2d71383bad4e80cde035fa684518969e125d4164
Nicolas <nicolasdemaria4@gmail.com>
2022-11-24 20:07:41 -0300

    Update sortArray.py

--- sortArray.py
+++ sortArray.py
@@ -12,10 +12,12 @@ Your function would return the following array:
 ["Eyes", "Glasses", "Monocles", "Telescopes"]
 
 All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
-
-
 '''
 
 
 def sort_by_length(arr):
-    pass
+    return sorted(arr, key=len)
+
+
+sort_by_length(["Telescopes", "Glasses", "Eyes", "Monocles"]
+               )
